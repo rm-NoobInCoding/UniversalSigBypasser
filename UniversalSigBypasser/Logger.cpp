09c2cadfd54fbda0d9c7da158786cf0b3381e5c0
NoobInCoding <35840559+rm-NoobInCoding@users.noreply.github.com>
2025-08-14 15:01:48 +0330

    Patch signature function to return true

--- UniversalSigBypasser/Logger.cpp
+++ UniversalSigBypasser/Logger.cpp
@@ -1,4 +1,4 @@
-#include "logger.h"
+#include "Logger.h"
 
 
 const char* LogLevelToString(LogLevel level) {
--- UniversalSigBypasser/SignalScanner.h
+++ UniversalSigBypasser/SignalScanner.h
@@ -1,35 +1,36 @@
 #include <Windows.h>
 #include <Psapi.h>
+#include <cstring>
 
-MODULEINFO GetModuleInfo(char* szModule)
+MODULEINFO GetModuleInfo(const char* szModule)
 {
-	MODULEINFO modinfo = { 0 };
-	HMODULE hModule = GetModuleHandleA(szModule);
-	if (hModule == 0)
-		return modinfo;
-	GetModuleInformation(GetCurrentProcess(), hModule, &modinfo, sizeof(MODULEINFO));
-	return modinfo;
+        MODULEINFO modinfo = { 0 };
+        HMODULE hModule = GetModuleHandleA(szModule);
+        if (!hModule)
+                return modinfo;
+        GetModuleInformation(GetCurrentProcess(), hModule, &modinfo, sizeof(MODULEINFO));
+        return modinfo;
 }
 
-DWORD64 FindPattern(char* module, char* pattern, char* mask)
+DWORD64 FindPattern(const char* module, const char* pattern, const char* mask)
 {
-	MODULEINFO mInfo = GetModuleInfo(module);
-	DWORD64 base = (DWORD64)mInfo.lpBaseOfDll;
-	DWORD64 size = (DWORD64)mInfo.SizeOfImage;
-	DWORD patternLength = (DWORD)strlen(mask);
+        MODULEINFO mInfo = GetModuleInfo(module);
+        DWORD64 base = (DWORD64)mInfo.lpBaseOfDll;
+        DWORD64 size = (DWORD64)mInfo.SizeOfImage;
+        DWORD patternLength = (DWORD)strlen(mask);
 
-	for (DWORD i = 0; i < size - patternLength; i++)
-	{
-		bool found = true;
-		for (DWORD j = 0; j < patternLength; j++)
-		{
-			found &= mask[j] == '?' || pattern[j] == *(char*)(base + i + j);
-		}
-		if (found)
-		{
-			return base + i;
-		}
-	}
+        for (DWORD i = 0; i < size - patternLength; i++)
+        {
+                bool found = true;
+                for (DWORD j = 0; j < patternLength; j++)
+                {
+                        found &= mask[j] == '?' || pattern[j] == *(char*)(base + i + j);
+                }
+                if (found)
+                {
+                        return base + i;
+                }
+        }
 
-	return -1;
+        return 0;
 }
